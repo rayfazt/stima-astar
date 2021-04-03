dcd190d0a0541a8797e0e6d8d43eb4f60402e88d
alghi <>
2021-04-03 18:11:37 +0000

    astar otw

--- astar.py
+++ astar.py
@@ -1,120 +1,77 @@
-class Node():
-    """A node class for A* Pathfinding"""
+import math
 
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
+class Graph:
+  def __init__(self):
+    self.components = {}
 
-        self.g = 0
-        self.h = 0
-        self.f = 0
+  def loadFile(self, filename):
+    try:
+      f = open(filename, 'r')
+      lines = f.readlines()
+      for i in range(len(lines)):
+        lines[i] = lines[i].replace("\n", "")
+          
+      # Ambil jumlah simpul
+      count_nodes = int(lines[0])
+      # Ambil nama simpul
+      nodes = []  
+      for i in range(1, count_nodes + 1):
+        dot = lines[i].split(" ")
+        nodes.append(dot[2])
+      # Ambil koordinat
+      coordinates = []
+      for i in range(1, count_nodes + 1):
+        dot = lines[i].split(" ")
+        coordinates.append((int(dot[0]), int(dot[1])))
+      # Ambil adjacent list
+      adj = []
+      for i in range(count_nodes + 1, len(lines)):
+        dot = lines[i].split(" ")
+        tmp = []
+        for val in dot:
+          tmp.append(int(val))
+        adj.append(tmp)
+          
+      for i in range(count_nodes):
+        if (nodes[i] not in self.components):
+          weight = []
+          for j in range(count_nodes):
+            if (adj[i][j]):
+              weight.append(self.euclideanDistance(coordinates[i],coordinates[j]))
+            else:
+              weight.append(0)
+          value = [coordinates[i],adj[i],weight]
+          self.components[nodes[i]] = value
+    except:
+      print("error: file not found")
 
-    def __eq__(self, other):
-        return self.position == other.position
+  def euclideanDistance(self, startPosition, targetPosition):
+    return math.sqrt( (startPosition[0]-targetPosition[0])**2 + (startPosition[1]-targetPosition[1])**2 )
 
+  def heuristic(self,current,target):
+    return self.euclideanDistance(self.components.get(current)[0],self.components.get(target)[0])
 
-def astar(maze, start, end):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+  def astar(self,root,target):
+    visited = set()
+    queue = []
 
-    # Create start and end node
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
+    visited.add(root)
+    queue.append(root)
+    while (queue != 0):
+      current = root
+      fn = {}
+      if(queue[0] == target):
+        break
+      else:
+        # f(n) = g(n)+h(n)
+        for i in range(len(self.components.get(current)[1])):
+          fn[current] = self.components.get(current)[1][i]+ self.heuristic(current,target)
+        
+        # Sort
 
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
-
-    # Add the start node
-    open_list.append(start_node)
-
-    # Loop until you find the end
-    while len(open_list) > 0:
-
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-
-        # Found the goal
-        if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
-
-        # Generate children
-        children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
-
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
-                continue
-
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
-                continue
-
-            # Create new node
-            new_node = Node(current_node, node_position)
-
-            # Append
-            children.append(new_node)
-
-        # Loop through children
-        for child in children:
-
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
-
-            # Create the f, g, and h values
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
-
-            # Add the child to the open list
-            open_list.append(child)
-
-
-def main():
-
-    maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
-    start = (0, 0)
-    end = (7, 6)
-
-    path = astar(maze, start, end)
-    print(path)
-
-
-if __name__ == '__main__':
-    main()
+g = Graph()
+filename = str(input("Masukkan nama file: "))
+g.loadFile(filename)
+root = str(input("Masukkan simpul awal: "))
+target = str(input("Masukkan simpul target: "))
+g.astar(root, target)
